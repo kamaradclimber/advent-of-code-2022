106c3102821b3ff4e83ce429138eb23f12f3a0a5
Grégoire Seux <g.seux@criteo.com>
2022-12-02 09:38:47 +0100

    ✨ Day 2 part 1
    Solution is a hardcoding of all 9 possibilities because I don't know yet
    how to split a string to make the real decomposition. Part 2 will likely
    require to change this ;)
    
    Change-Id: Ia0d07553d6d18640c10a9a32881d305154151a72

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,38 @@ fn main() {
     let input_file = &args[3];
     println!("Solving day {day}, part {part} with input file {input_file}");
 
-    match part {
-        1 => day1(input_file.to_string(), 1),
-        2 => day1(input_file.to_string(), 3),
-        _ => panic!("There are only 2 part during each day, you gave {part}"),
+    match (day, part) {
+        (1, 1) => day1(input_file.to_string(), 1),
+        (1, 2) => day1(input_file.to_string(), 3),
+        (2, 1) => day2(input_file.to_string()),
+        (_, 1) => panic!("Solution for day {day} has not been implemented yet"),
+        (_, 2) => panic!("Solution for day {day} has not been implemented yet"),
+        (_, _) => panic!("There are only 2 parts per day"),
     }
 }
 
-fn day1(input_file: String, top: u8) {
-    println!("Starting to solve day1");
+fn day2(input_file: String) {
+    let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
+    let lines = contents.lines();
+    let mut score = 0;
+    for line in lines {
+        match line {
+            "A X" => score += 1 + 3,
+            "A Y" => score += 2 + 6,
+            "A Z" => score += 3 + 0,
+            "B X" => score += 1 + 0,
+            "B Y" => score += 2 + 3,
+            "B Z" => score += 3 + 6,
+            "C X" => score += 1 + 6,
+            "C Y" => score += 2 + 0,
+            "C Z" => score += 3 + 3,
+            _ => panic!("Unknown line {line}"),
+        }
+    }
+    println!("Score is {score}")
+}
 
+fn day1(input_file: String, top: u8) {
     let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
     let lines = contents.lines();
 
