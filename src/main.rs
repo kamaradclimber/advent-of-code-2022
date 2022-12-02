dd2f1f1b3995902a114184edc2e8f21cdb535bc1
Grégoire Seux <g.seux@criteo.com>
2022-12-02 10:03:37 +0100

    ✨ Day 2 part 2
    Some refactoring was necessary indeed. I don't think I would have made
    it simpler in 💎 to keep some readability.
    Alternative would have been to manipulate arrays of arrays and reverse
    them all around.
    
    Change-Id: Ib1f0ae48d306f77b106de9f8343b130ce0981510

--- src/main.rs
+++ src/main.rs
@@ -17,29 +17,82 @@ fn main() {
     match (day, part) {
         (1, 1) => day1(input_file.to_string(), 1),
         (1, 2) => day1(input_file.to_string(), 3),
-        (2, 1) => day2(input_file.to_string()),
+        (2, 1) => day2(input_file.to_string(), 1),
+        (2, 2) => day2(input_file.to_string(), 2),
         (_, 1) => panic!("Solution for day {day} has not been implemented yet"),
         (_, 2) => panic!("Solution for day {day} has not been implemented yet"),
         (_, _) => panic!("There are only 2 parts per day"),
     }
 }
 
-fn day2(input_file: String) {
+enum Play {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn day2_score(player1: Play, player2: Play) -> u32 {
+    match (player1, player2) {
+        (Play::Rock, Play::Rock) => 1 + 3,
+        (Play::Rock, Play::Paper) => 2 + 6,
+        (Play::Rock, Play::Scissors) => 3 + 0,
+        (Play::Paper, Play::Rock) => 1 + 0,
+        (Play::Paper, Play::Paper) => 2 + 3,
+        (Play::Paper, Play::Scissors) => 3 + 6,
+        (Play::Scissors, Play::Rock) => 1 + 6,
+        (Play::Scissors, Play::Paper) => 2 + 0,
+        (Play::Scissors, Play::Scissors) => 3 + 3,
+    }
+}
+
+enum Outcome {
+    LOOSE,
+    DRAW,
+    WIN,
+}
+
+fn day2_strategy(player1: &Play, desired_outcome: Outcome) -> Play {
+    match (player1, desired_outcome) {
+        (Play::Rock, Outcome::DRAW) => Play::Rock,
+        (Play::Rock, Outcome::LOOSE) => Play::Scissors,
+        (Play::Rock, Outcome::WIN) => Play::Paper,
+        (Play::Paper, Outcome::DRAW) => Play::Paper,
+        (Play::Paper, Outcome::LOOSE) => Play::Rock,
+        (Play::Paper, Outcome::WIN) => Play::Scissors,
+        (Play::Scissors, Outcome::DRAW) => Play::Scissors,
+        (Play::Scissors, Outcome::LOOSE) => Play::Paper,
+        (Play::Scissors, Outcome::WIN) => Play::Rock,
+    }
+}
+
+fn day2(input_file: String, part: u8) {
     let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
     let lines = contents.lines();
     let mut score = 0;
     for line in lines {
-        match line {
-            "A X" => score += 1 + 3,
-            "A Y" => score += 2 + 6,
-            "A Z" => score += 3 + 0,
-            "B X" => score += 1 + 0,
-            "B Y" => score += 2 + 3,
-            "B Z" => score += 3 + 6,
-            "C X" => score += 1 + 6,
-            "C Y" => score += 2 + 0,
-            "C Z" => score += 3 + 3,
-            _ => panic!("Unknown line {line}"),
+        let player1 = match &line[0..1] {
+            "A" => Play::Rock,
+            "B" => Play::Paper,
+            "C" => Play::Scissors,
+            _ => panic!("Invalid line {line}"),
+        };
+        if part == 1 {
+            let player2 = match &line[2..3] {
+                "X" => Play::Rock,
+                "Y" => Play::Paper,
+                "Z" => Play::Scissors,
+                _ => panic!("Invalid line {line}"),
+            };
+            score += day2_score(player1, player2);
+        } else {
+            let desired = match &line[2..3] {
+                "X" => Outcome::LOOSE,
+                "Y" => Outcome::DRAW,
+                "Z" => Outcome::WIN,
+                _ => panic!("Invalid line {line}"),
+            };
+            let player2 = day2_strategy(&player1, desired);
+            score += day2_score(player1, player2);
         }
     }
     println!("Score is {score}")
