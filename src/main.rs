1f4203f6844b43a3a075cb30f5a70dbc17f3f7ad
Grégoire Seux <g.seux@criteo.com>
2022-12-05 10:23:35 +0100

    ✨ Day 5 part 1
    More difficulties than usual with borrowing.
    Also some difficulties to make indexed access to string characters.
    
    Change-Id: Ibc79f81ade5e19f478035919452698a8a78bb162

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,68 @@ fn main() {
         (3, 2) => day3(input_file.to_string(), 2),
         (4, 1) => day4(input_file.to_string(), 1),
         (4, 2) => day4(input_file.to_string(), 2),
+        (5, 1) => day5(input_file.to_string(), 1),
         (_, 1) => panic!("Solution for day {day} has not been implemented yet"),
         (_, 2) => panic!("Solution for day {day} has not been implemented yet"),
         (_, _) => panic!("There are only 2 parts per day"),
     }
+}
+
+fn day5(input_file: String, part: u8) {
+    let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
+    let mut lines = contents.lines();
+    let nb_columns = 30;
+    let mut stacks = vec![Vec::new(); nb_columns + 1];
+
+    while let Some(line) = lines.next() {
+        if line == "" {
+            break;
+        }
+        let chars = line.as_bytes();
+        if let Some(el) = chars.get(1) {
+            let char = *el as char;
+            if char == '1' {
+                continue;
+            }
+        }
+        for column_id in 1..=nb_columns {
+            let idx = 1 + (column_id - 1) * 4;
+            let my_crate = chars.get(idx);
+            if let Some(my_crate) = my_crate {
+                let real_char = *my_crate as char;
+                if real_char != ' ' {
+                    stacks[column_id].push(real_char);
+                }
+            }
+        }
+    }
+    for column_id in 1..=nb_columns {
+        stacks[column_id].reverse();
+    }
+    let re = Regex::new(r"^move (\d+) from (\d+) to (\d+)$").unwrap();
+    for line in lines {
+        let instruction = re.captures(line).unwrap();
+        let count = &instruction[1].parse::<u32>().unwrap();
+        let src_column_id = &instruction[2].parse::<usize>().unwrap();
+        let dst_column_id = &instruction[3].parse::<usize>().unwrap();
+        for _ in 0..*count {
+            match stacks[*src_column_id].pop() {
+                Some(my_crate) => {
+                    stacks[*dst_column_id].push(my_crate);
+                }
+                None => panic!("Stack is empty, cannot move element from it!"),
+            }
+        }
+    }
+    let mut stack_top = Vec::new();
+    for mut stack in stacks {
+        match stack.pop() {
+            Some(my_crate) => stack_top.push(my_crate),
+            None => (),
+        }
+    }
+    let output: String = stack_top.iter().collect();
+    println!("Solution is {output}");
 }
 
 fn includes_range(
