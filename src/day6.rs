2c0236b70057c9edc60ffb182cf97642bd887eb6
Grégoire Seux <g.seux@criteo.com>
2022-12-06 10:39:33 +0100

    ✨ Day 6 part 1
    Thanks @anisse for the tips of using itertools :)
    
    Change-Id: I9be9b769bf3cb0b79c9800c5a2e6f9abaaa8eddb

--- src/day6.rs
+++ src/day6.rs
@@ -1,12 +1,23 @@
+use itertools::Itertools;
 use std::fs;
 
 pub fn solve(input_file: String, part: u8) {
     let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
     let lines = contents.lines();
-    let mut result = 0;
     for line in lines {
-        result += 1;
-        println!("Line is {0}", &line);
+        let index = first_all_different_slice(&line, 4);
+        println!("Line is {0}, first index {1}", &line, index);
     }
-    println!("Score for part {0}, is {result}", part);
+}
+
+fn first_all_different_slice(input: &str, size: usize) -> usize {
+    let char_vec: Vec<char> = input.chars().collect(); // <- this is the trick to manipulate
+                                                       // strings as list of chars easily
+    let iter = char_vec.windows(size);
+    for (index, slice) in iter.enumerate() {
+        if slice.iter().all_unique() {
+            return index + 4;
+        }
+    }
+    panic!("Found no unique sequence of {size} in this string");
 }
