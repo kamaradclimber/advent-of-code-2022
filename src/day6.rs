3dac42a8deac4de47ae7d74ec34cde5c88779137
Grégoire Seux <g.seux@criteo.com>
2022-12-06 10:42:53 +0100

    ✨ Day 6 part 2
    This one was easy. i guess it was only to filter out solutions that were
    testing all combinations to implement uniqueness.
    
    Change-Id: I3b5717c8e9c8cec11484f5e23ff284068f96328a

--- src/day6.rs
+++ src/day6.rs
@@ -4,8 +4,9 @@ use std::fs;
 pub fn solve(input_file: String, part: u8) {
     let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
     let lines = contents.lines();
+    let header_size = if part == 1 { 4 } else { 14 };
     for line in lines {
-        let index = first_all_different_slice(&line, 4);
+        let index = first_all_different_slice(&line, header_size);
         println!("Line is {0}, first index {1}", &line, index);
     }
 }
@@ -16,7 +17,7 @@ fn first_all_different_slice(input: &str, size: usize) -> usize {
     let iter = char_vec.windows(size);
     for (index, slice) in iter.enumerate() {
         if slice.iter().all_unique() {
-            return index + 4;
+            return index + size;
         }
     }
     panic!("Found no unique sequence of {size} in this string");
