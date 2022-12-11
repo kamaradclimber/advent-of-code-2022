bd37e1c6b9a8aa16b522c75fdbe22468b4ca8970
Grégoire Seux <kamaradclimber@gmail.com>
2022-12-11 15:23:45 +0100

    ✨ Day 11 part 2
    Change-Id: I44f6e212efd0a8686fb2d598f09d4b1806de2c4d

--- src/day11.rs
+++ src/day11.rs
@@ -1,34 +1,44 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 
 pub fn solve(input_file: String, part: u8) {
     let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
-    let mut monkey_holdings : Vec<Vec<Item>> = vec![];
-    let mut monkeys : Vec<Monkey> = vec![];
+    let mut monkey_holdings: Vec<Vec<Item>> = vec![];
+    let mut monkeys: Vec<Monkey> = vec![];
     let blocks = contents.split("\n\n");
     for (id, block) in blocks.enumerate() {
-        let monkey_lines : Vec<&str> = block.lines().collect();
+        let monkey_lines: Vec<&str> = block.lines().collect();
         monkey_holdings.push(parse_item_line(&monkey_lines[1]));
         let operation = monkey_lines[2].trim().parse::<ItemOperation>().unwrap();
-        let test = monkey_lines[3..6].join("\n").parse::<ItemTestDestination>().unwrap();
-        let monkey = Monkey { id, operation, test };
+        let test = monkey_lines[3..6]
+            .join("\n")
+            .parse::<ItemTestDestination>()
+            .unwrap();
+        let monkey = Monkey {
+            id,
+            operation,
+            test,
+        };
         monkeys.push(monkey);
     }
-    let mut inspected_items : Vec<u32> = vec![0; monkeys.len()];
+    let mut inspected_items: Vec<u64> = vec![0; monkeys.len()];
 
     let round = if part == 1 { 20 } else { 10000 };
+
+    // technically we could take the gcd of all those numbers but they are prime numbers 🤯
+    let ring: u64 = monkeys.iter().map(|m| m.test.divide_criteria).product();
+    println!("🤓 Using {ring} as a way to keep worry level manageable");
 
     for round in 1..=round {
         for monkey in monkeys.iter() {
             let holdings = &monkey_holdings[monkey.id];
-            let mut new_holdings : Vec<Vec<Item>> = vec![vec![]; monkey_holdings.len()];
+            let mut new_holdings: Vec<Vec<Item>> = vec![vec![]; monkey_holdings.len()];
             for item in holdings {
                 inspected_items[monkey.id] += 1;
-                let initial_worry_level = *item;
-                let new_item = if part ==1 {
+                let new_item = if part == 1 {
                     monkey.operation.run(*item) / 3
                 } else {
-                    *item
+                    Item(monkey.operation.run(*item) % ring)
                 };
                 let dest_id = if (new_item) % monkey.test.divide_criteria == 0 {
                     monkey.test.destination_if_true
@@ -40,62 +50,69 @@ pub fn solve(input_file: String, part: u8) {
             }
             monkey_holdings[monkey.id].clear();
             // TODO: find a way to avoid this intermediate structure to pass transferred items
-            for id in vec![monkey.test.destination_if_true, monkey.test.destination_if_false] {
+            for id in vec![
+                monkey.test.destination_if_true,
+                monkey.test.destination_if_false,
+            ] {
                 monkey_holdings[id].append(&mut new_holdings[id]);
             }
         }
         if round == 1 {
-            inspect_monkey_actions(round, &inspected_items);
+            inspect_monkey_actions(round, &inspected_items, &monkey_holdings);
         }
         if round == 20 {
-            inspect_monkey_actions(round, &inspected_items);
+            inspect_monkey_actions(round, &inspected_items, &monkey_holdings);
         }
         if round % 1000 == 0 {
-            inspect_monkey_actions(round, &inspected_items);
+            inspect_monkey_actions(round, &inspected_items, &monkey_holdings);
         }
     }
     inspected_items.sort();
-    let inspected_items : Vec<&u32> = inspected_items.iter().rev().collect();
+    let inspected_items: Vec<&u64> = inspected_items.iter().rev().collect();
     let monkey_business_level = inspected_items[0] * inspected_items[1];
     println!("Level of monkey business is {monkey_business_level}");
-
 }
 
-fn inspect_monkey_actions(round: u32, inspected_items: &Vec<u32>) {
+fn inspect_monkey_actions(
+    round: u64,
+    inspected_items: &Vec<u64>,
+    monkey_holdings: &Vec<Vec<Item>>,
+) {
     println!("== After round {round} ==");
     for (id, item_count) in inspected_items.iter().enumerate() {
         println!("Monkey {0} inspect items {1} times.", id, item_count);
+        // dbg!(&monkey_holdings[id]);
     }
 }
 
 fn parse_item_line(line: &str) -> Vec<Item> {
     let re = Regex::new(r"^Starting items: (.+)$").unwrap();
     let cap = re.captures(line.trim()).unwrap();
-    let numbers : Vec<&str> = cap[1].split(", ").collect();
+    let numbers: Vec<&str> = cap[1].split(", ").collect();
     let mut my_numbers = vec![];
     for n in numbers {
-        let i : Item = Item(n.parse::<u32>().unwrap());
+        let i: Item = Item(n.parse::<u64>().unwrap());
         my_numbers.push(i);
     }
     my_numbers
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Item(u32);
+struct Item(u64);
 
-impl std::ops::Add<u32> for Item {
+impl std::ops::Add<u64> for Item {
     type Output = Self;
 
-    fn add(self, rhs: u32) -> Self::Output {
+    fn add(self, rhs: u64) -> Self::Output {
         let Item(worry_level) = self;
         Item(worry_level + rhs)
     }
 }
 
-impl std::ops::Mul<u32> for Item {
+impl std::ops::Mul<u64> for Item {
     type Output = Self;
 
-    fn mul(self, rhs: u32) -> Self::Output {
+    fn mul(self, rhs: u64) -> Self::Output {
         let Item(worry_level) = self;
         Item(worry_level * rhs)
     }
@@ -109,18 +126,18 @@ impl std::ops::Mul<Item> for Item {
     }
 }
 
-impl std::ops::Div<u32> for Item {
+impl std::ops::Div<u64> for Item {
     type Output = Self;
 
-    fn div(self, rhs: u32) -> Self::Output {
+    fn div(self, rhs: u64) -> Self::Output {
         let Item(worry_level) = self;
         Item(worry_level / rhs)
     }
 }
-impl std::ops::Rem<u32> for Item {
-    type Output = u32;
+impl std::ops::Rem<u64> for Item {
+    type Output = u64;
 
-    fn rem(self, rhs: u32) -> Self::Output {
+    fn rem(self, rhs: u64) -> Self::Output {
         let Item(worry_level) = self;
         worry_level % rhs
     }
@@ -129,8 +146,8 @@ impl std::ops::Rem<u32> for Item {
 #[derive(Debug)]
 enum ItemOperation {
     Square,
-    Add(u32),
-    Mul(u32)
+    Add(u64),
+    Mul(u64),
 }
 
 impl ItemOperation {
@@ -153,27 +170,26 @@ impl std::str::FromStr for ItemOperation {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if s == "Operation: new = old * old" {
-
-            return Ok(ItemOperation::Square)
+            return Ok(ItemOperation::Square);
         }
         let re = Regex::new(r"^Operation: new = old (.) (\d+)$").unwrap();
         if !re.is_match(s) {
-            return Err(InstructionParsingError::UnknownInstruction)
+            return Err(InstructionParsingError::UnknownInstruction);
         }
         let cap = re.captures(s).unwrap();
-        let operand : u32 = cap[2].parse().unwrap();
+        let operand: u64 = cap[2].parse().unwrap();
         let operator = &cap[1];
         match operator {
             "+" => Ok(ItemOperation::Add(operand)),
             "*" => Ok(ItemOperation::Mul(operand)),
-            _ => Err(InstructionParsingError::UnknownInstruction)
+            _ => Err(InstructionParsingError::UnknownInstruction),
         }
     }
 }
 
 #[derive(Debug)]
 struct ItemTestDestination {
-    divide_criteria: u32,
+    divide_criteria: u64,
     destination_if_true: usize,
     destination_if_false: usize,
 }
@@ -184,13 +200,17 @@ impl std::str::FromStr for ItemTestDestination {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let re = Regex::new(r"^Test: divisible by (\d+)\n.+If true: throw to monkey (\d+)\n.+If false: throw to monkey (\d+)$").unwrap();
         if !re.is_match(s.trim()) {
-            return Err(InstructionParsingError::UnknownInstruction)
+            return Err(InstructionParsingError::UnknownInstruction);
         }
         let numbers = re.captures(s.trim()).unwrap();
-        let divide_criteria : u32 = numbers[1].parse().unwrap();
-        let destination_if_true : usize = numbers[2].parse().unwrap(); 
-        let destination_if_false : usize = numbers[3].parse().unwrap(); 
-        Ok(ItemTestDestination { divide_criteria, destination_if_true, destination_if_false })
+        let divide_criteria: u64 = numbers[1].parse().unwrap();
+        let destination_if_true: usize = numbers[2].parse().unwrap();
+        let destination_if_false: usize = numbers[3].parse().unwrap();
+        Ok(ItemTestDestination {
+            divide_criteria,
+            destination_if_true,
+            destination_if_false,
+        })
     }
 }
 
