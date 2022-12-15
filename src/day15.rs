880aa83b35702932c4b0d4a29947f69472c52ba6
Grégoire Seux <kamaradclimber@gmail.com>
2022-12-15 10:25:39 +0100

    WIP part 2
    Change-Id: Ic2dae6ea321b670a4f90653f77f6bb8d92656d05

--- src/day15.rs
+++ src/day15.rs
@@ -4,7 +4,7 @@ use std::collections::{HashMap,HashSet};
 
 pub fn solve(input_file: String, part: u8) {
     let contents = fs::read_to_string(&input_file).expect("Could not read input_file");
-    let considered_line = 2000000;
+    let considered_line = 10;
     println!("Considered line is **{considered_line}**");
     let lines = contents.lines();
     let mut readings = HashMap::new();
@@ -21,22 +21,69 @@ pub fn solve(input_file: String, part: u8) {
         sensor.distance(&beacon);
     }
 
-    let mut marked_points = HashSet::new();
-    let mut i = 0;
-    for (sensor, beacon) in &readings {
-        dbg!(i);
-        let d = sensor.distance(&beacon);
-        let impossible_positions = sensor
-            .points_within_in_line(d, considered_line)
-            .into_iter()
-            .filter(|p| p.y == considered_line )
-            .filter(|p| p != beacon );
-        for p in impossible_positions {
-            marked_points.insert(p);
+    if part == 1 {
+
+        let mut marked_points = HashSet::new();
+        let mut i = 0;
+        for (sensor, beacon) in &readings {
+            dbg!(i);
+            let d = sensor.distance(&beacon);
+            let impossible_positions = sensor
+                .points_within_in_line(d, considered_line)
+                .into_iter()
+                .filter(|p| p.y == considered_line )
+                .filter(|p| p != beacon );
+            for p in impossible_positions {
+                marked_points.insert(p);
+            }
+            i += 1;
         }
-        i += 1;
+        println!("There are {0} points on line {considered_line}", marked_points.len());
+    } else {
+        let rangey = 0..=400000;
+        let rangex = 0..=4000000;
+        for y in rangey {
+            if y % 4000 == 0 {
+              println!("Considering line {y}");
+            }
+            for p in free_positions(y, &rangex, &readings) {
+                println!("{p:?} is free");
+            }
+        }
     }
-    println!("There are {0} points on line {considered_line}", marked_points.len());
+}
+
+fn free_positions(y: i32, x_range: &std::ops::RangeInclusive<i32>, readings: &HashMap<Point,Point>) -> Vec<Point> {
+    let mut free_pos = vec![];
+    let mut x = *x_range.start();
+    while x <= *x_range.end() {
+        let current = Point { x, y };
+
+        // FIXME: maybe we could sort sensors from right to left to maximize our jump size 🤯
+        let within_reach = readings.iter().filter(|(sensor, beacon)| current.distance(sensor) <= sensor.distance(beacon));
+        let mut w = vec![];
+        for (sensor, _) in within_reach {
+            w.push(sensor);
+        }
+        w.sort_by(|s1,s2| s2.x.cmp(&s1.x));
+        match w.get(0) {
+            None => free_pos.push(current),
+            Some(sensor) => {
+                // then we know current point cannot be the missing beacon
+                // we also know we can progress on the line quite a while
+                if x < sensor.x {
+                    // println!("Moving from pos {x} to {0} because in the range of {1:?}", x + 2 * (sensor.x - x), sensor);
+                    x += 2 * (sensor.x - x);
+                } else {
+                    // println!("{0} sensors were in reach of this point ({current:?}). Example: {1:?}", w.len(), w.get(0).unwrap());
+                    let beacon = readings.get(*sensor).unwrap();
+                    x += (sensor.distance(beacon) - current.distance(sensor)) as i32;
+                }
+            }
+        }
+        x += 1;
+    }
+    free_pos
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
