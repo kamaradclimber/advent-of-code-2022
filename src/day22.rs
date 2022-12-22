40851c334d14e224dd4dd412ba02fefabd552b9d
Grégoire Seux <g.seux@criteo.com>
2022-12-22 20:35:05 +0100

    WIP
    Change-Id: Ieff1f4a11df06f50e5e25931b80f03954b6be7bf

--- src/day22.rs
+++ src/day22.rs
@@ -67,6 +67,29 @@ pub fn solve(input_file: String, part: u8) {
 
 }
 
+fn print(coords: &HashMap<Point, Tile>, edge_size: u32, point: Point, dir: Direction) {
+    for y in 0..=(edge_size*5) {
+        for x in 0..=(edge_size*5) {
+            let p = (x as i32, y as i32);
+            if p == point {
+                match dir { 
+                    Direction::Up => print!("^"),
+                    Direction::Down => print!("v"),
+                    Direction::Left => print!("<"),
+                    Direction::Right => print!(">"),
+                }
+            } else {
+                match coords.get(&p) {
+                    None => print!(" "),
+                    Some(Tile::Wall) => print!("#"),
+                    _ => print!("."),
+                }
+            }
+        }
+        println!("");
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 enum LastTurnType {
     Right,
@@ -75,62 +98,64 @@ enum LastTurnType {
 }
 
 fn folding(edge_size: u32, initial_point: Point, initial_direction: Direction, coords: &HashMap<Point,Tile>) -> HashMap<Point, Point> {
-    let mut stack : Vec<(Point, Direction, Option<LastTurnType>)> = vec![];
+    let mut edge_stack : Vec<(Point, Direction)> = vec![];
     let mut folding = HashMap::new();
-    let mut cur_pos = initial_point;
+    let mut cur_pos = (initial_point.0-1, initial_point.1);
     let mut cur_dir = initial_direction;
     let mut last_turn_type = None;
-    let mut step = 1;
+    edge_stack.push((initial_point, initial_direction));
     loop {
+        print(&coords, edge_size, cur_pos, cur_dir);
         println!("  at {0},{1} (0-indexed), dir {cur_dir:?}", cur_pos.0, cur_pos.1);
-        if folding.contains_key(&cur_pos) {
+        if last_turn_type.is_some() && cur_pos == initial_point {
             break;
         }
-        let my_ahead = next_pos(cur_pos, cur_dir);
-        let my_left = next_pos(cur_pos, cur_dir.left());
+        //let my_ahead = next_pos(cur_pos, cur_dir, edge_size+1);
+        let my_ahead = next_pos(cur_pos, cur_dir, 1);
+        let my_left = next_pos(my_ahead, cur_dir.left(), 1);
         match (coords.get(&my_ahead), coords.get(&my_left)) {
             (None, None) => {
+                cur_pos = next_pos(cur_pos, cur_dir.left().left(), 1);
+                cur_pos = next_pos(cur_pos, cur_dir, 1);
                 // change our direction
                 cur_dir = cur_dir.right();
                 // record last turn
                 last_turn_type = Some(LastTurnType::Right);
                 println!("== Turn right");
-                step = 0;
+                edge_stack.push((cur_pos, cur_dir));
             },
-            (None, Some(_)) => panic!("I am not sure this case should happen"),
+            (None, Some(_)) => panic!("I am not sure this case can/should happen"),
             (Some(_), Some(_)) => {
-                if step % edge_size == 0 {
-                    last_turn_type = Some(LastTurnType::Left);
-                    cur_dir = cur_dir.left();
-                    println!("== Turn left");
-                    step = 0;
-                } else {
-                    stack.push((cur_pos, cur_dir, last_turn_type));
-                }
+                cur_pos = next_pos(cur_pos, cur_dir, 1);
+                // change our direction
+                cur_dir = cur_dir.left();
+                last_turn_type = Some(LastTurnType::Left);
+                println!("== Turn left");
+                edge_stack.push((cur_pos, cur_dir));
             },
             (Some(_), None) => {
-                stack.push((cur_pos, cur_dir, last_turn_type));
-                if step % edge_size == 0 {
-                    last_turn_type = Some(LastTurnType::Straight);
-                    println!("== Continue straight ahead");
-                    step = 0;
-                }
+                cur_pos = next_pos(cur_pos, cur_dir, 1);
+                last_turn_type = Some(LastTurnType::Straight);
+                println!("== Continue straight ahead");
             },
         }
-        let (_, _, turn_type) = stack.last().unwrap();
-        match turn_type {
+        match last_turn_type {
             None => (),
             Some(LastTurnType::Left) => {
-                let (p1, _, _) = stack.pop().unwrap();
-                let (p2, _, _) = stack.pop().unwrap();
-                println!("{0:?} <-> {1:?}", p1,p2);
-                folding.insert(p1,p2);
-                folding.insert(p2,p1);
+                let (p1, dir1) = edge_stack.pop().unwrap();
+                let (p2, dir2) = edge_stack.pop().unwrap();
+                let mut pp1 = p1;
+                let mut pp2 = p2;
+                for step in 1..edge_size {
+                    let a= next_pos(p1, dir1, step);
+                    let b = next_pos(p2, dir2.left().left(), edge_size - step);
+                    println!("{0:?} <-> {1:?}", a, b);
+                    folding.insert(a,b);
+                    folding.insert(b,a);
+                }
             },
             _ => (), // nothing for now
         }
-        cur_pos = next_pos(cur_pos, cur_dir);
-        step += 1;
     }
 
     folding
@@ -147,20 +172,23 @@ fn move_wrap(pos: Point, direction: Direction, lines: &Vec<(i32,i32)>, columns: 
 }
 
 // return a point that might not be on the board
-fn next_pos(pos: Point, direction: Direction) -> Point {
-    let (x,y) = (pos.0 as i32, pos.1 as i32);
-    match direction {
-        Direction::Up => (x, y - 1),
-        Direction::Left => (x - 1, y),
-        Direction::Down => (x, y + 1),
-        Direction::Right => (x + 1, y),
+fn next_pos(pos: Point, direction: Direction, step_size: u32) -> Point {
+    let (mut x,mut y) = (pos.0 as i32, pos.1 as i32);
+    for _ in 0..step_size {
+        (x, y) = match direction {
+            Direction::Up => (x, y - 1),
+            Direction::Left => (x - 1, y),
+            Direction::Down => (x, y + 1),
+            Direction::Right => (x + 1, y),
+        }
     }
+    (x,y)
 }
 
 fn move_one_step(pos: Point, direction: Direction, coords: &HashMap<Point, Tile>,
                  lines: &Vec<(i32,i32)>, columns: &Vec<(i32,i32)>) -> Point {
     // get a candidate new pos
-    let mut new_pos = next_pos(pos, direction);
+    let mut new_pos = next_pos(pos, direction, 1);
     // wrap it, undo it or keep it
     match coords.get(&new_pos) {
         None => { // wrap
